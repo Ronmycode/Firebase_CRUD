7c8963608b8ac70e897fa21ca3d81e1f4de43100
Ronmycode <67519456+Ronmycode@users.noreply.github.com>
2024-11-01 20:56:18 -0600

    deleted dist folder and re-test

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -4,4 +4,9 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
+  esbuild: {
+    loader: "jsx", // Ensures Vite processes files with JSX syntax correctly
+    include: /src\/.*\.jsx?$/, // Only target files in `src` with .js or .jsx
+  },
 });
